bad27485c45d69f070fa06f9bc50f19764c6af9d
eric mcgregor <eric.mcgregor@rackspace.com>
2013-11-10 14:41:27 -0600

    nearly completed new site activity

--- app/modules/newsite.js
+++ app/modules/newsite.js
@@ -4,13 +4,15 @@ angular.module('myApp.newsite', [])
 .factory('wpOptions', function(){
 	return {
 		url:'',
+    siteTitle:'',
+    description:'',
 		pages:[],
-        themes:[],
-        plugins:[],
-        users:[],
-        menus:[],
-        options:[],
-        wpusers:[],
+    themes:[],
+    plugins:[],
+    admin:{},
+    menus:[],
+    options:[],
+    wpusers:[],
 	}
 })
 
@@ -21,14 +23,36 @@ angular.module('myApp.newsite', [])
       restrict:"A",
       scope:{},
       link: function(scope, element, attrs) {
+
         scope.Data = Data;
-        scope.options = wpOptions;
+        scope.options = angular.copy(wpOptions);
+        console.log(wpOptions)
         scope.offCanvasService = offCanvasService;
         console.log(scope)
 
 
         scope.addItem = function(obj, location) {
           location.push(angular.copy(obj))
+        }
+
+        scope.createSite = function() {
+          if(scope.options.themes[0])
+          scope.options.image = scope.options.themes[0].image;
+          console.log(scope.options)
+
+          Data.sites.mySites.push(angular.copy(scope.options));
+
+
+
+          offCanvasService.showDetail = !offCanvasService.showDetail;
+          offCanvasService.detail.template = 'app/templates/modals/site.detail.html';
+          offCanvasService.detail.input = angular.copy(scope.options);
+          offCanvasService.detail.output = [];
+          scope.$apply();
+
+                    scope.options = angular.copy(wpOptions);
+
+
         }
 
 
